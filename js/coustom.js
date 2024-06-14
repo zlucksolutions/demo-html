ae4e42f47d2978de3350762ed3473a5c62cba5db
Maulik <maulik@zluck.com>
2024-06-14 17:35:33 +0530

    Initial Commit

---
+++ js/coustom.js
@@ -0,0 +1,13 @@
+$(".slick-slider").slick({
+  slidesToShow: 1,
+  infinite: false,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true,
+  Boolean,
+  arrows: false,
+  Boolean,
+});
+
+
